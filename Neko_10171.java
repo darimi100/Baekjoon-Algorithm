23c68a2efb276301f8de4ffe22587a955b8ec14b
darimi100 <105377694+darimi100@users.noreply.github.com>
2022-07-12 20:46:58 +0900

    10171번 : 고양이

---
+++ Neko_10171.java
@@ -0,0 +1,8 @@
+class Neko_10171 {
+    public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.print(" \\(__)|");
+    }
+}
