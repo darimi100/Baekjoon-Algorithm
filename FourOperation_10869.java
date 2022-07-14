d15deadb5260c4bc06e92784ac95d2ac21af0c7e
darimi100 <105377694+darimi100@users.noreply.github.com>
2022-07-15 00:11:40 +0900

    사칙연산 : 10869번

---
+++ FourOperation_10869.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class FourOperation_10869 {
+    public static void main(String args[]){
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
