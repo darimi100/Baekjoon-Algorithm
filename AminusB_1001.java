e9a1f67824a8c806c1c1680ef8b87992b00c096c
darimi100 <105377694+darimi100@users.noreply.github.com>
2022-07-14 21:04:44 +0900

    1000번 : A+B 1001번 : A-B 10998번 : A*B 1008번 : A/B

---
+++ AminusB_1001.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class AminusB_1001 {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner s = new Scanner(System.in);
+        a = s.nextInt();
+        b = s.nextInt();
+        System.out.print(a-b);
+    }
+}
---
+++ AplusB_1000.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class AplusB_1000 {
+
+    public static void main(String[] args) {
+        int a, b;
+        Scanner s = new Scanner(System.in);
+        a = s.nextInt();
+        b = s.nextInt();
+        System.out.print(a+b);
+    }
+}
