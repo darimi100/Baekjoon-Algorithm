72ffcafcd601d27a9fade4c920ec0b632e23e1b3
darimi100 <105377694+darimi100@users.noreply.github.com>
2022-07-16 22:28:45 +0900

    ??! : 10926번

---
+++ IdQuestion_10926.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class IdQuestion_10926 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next() + "??!";
+        System.out.print(a);
+        sc.close(); //스캐너 사용 후 닫기
+    }
+}
