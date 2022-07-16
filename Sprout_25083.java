3b93562fa842a9f4ae6772197294dcc0579572e8
darimi100 <105377694+darimi100@users.noreply.github.com>
2022-07-17 00:33:32 +0900

    25083번 : 새싹

---
+++ Sprout_25083.java
@@ -0,0 +1,13 @@
+public class Sprout_25083 {
+    public static void main(String[] args){
+        System.out.println("         ,r\'\"7");
+        System.out.println("r`-_   ,\'  ,/");
+        System.out.println(" \\. \". L_r\'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.print("      |");
+    }
+}
+
+// 이스케이프 문자(개행 문자)
+// \\, \', \", \t, \n, ...
