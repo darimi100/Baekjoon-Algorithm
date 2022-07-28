1ccd64cd1e8d4ab05bc55892240beda4d4f4d37e
darimi100 <105377694+darimi100@users.noreply.github.com>
2022-07-28 19:43:36 +0900

    클래스 이름 변경

--- AplusB_5_10952.java
+++ AplusB_5_10952.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Main {
+class AplusB_5_10952 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         boolean isZero = false;
